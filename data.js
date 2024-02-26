4d0556ecfa6a8e3445b28a962d534e4c353f3eb0
RohanInCode07 <160272455+RohanInCode07@users.noreply.github.com>
2024-02-26 12:42:01 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,313 @@
+const items =[
+{
+  id: '001',
+  image : 'images/Clothes_For_Men/c4.jpeg',
+  company : 'Marcon',
+  item_name : 'Cotton Shirt ',
+  current_price : 600,
+  original_price : 1045,
+  discount_percentage : 41,
+  return_period : 12,
+  delivery_date : '12 Mar 2024',
+  rating : {
+    stars : 4.5 ,
+    noOfReviews : 1500,
+    },
+},
+
+{
+id: '002',
+image : 'images/Clothes_For_Men/c2.jpeg',
+company : 'Marcon',
+item_name : 'Cotton Shirt ',
+current_price : 600,
+original_price : 1045,
+discount_percentage : 41,
+return_period : 12,
+delivery_date : '12 Mar 2024',
+rating : {
+  stars : 4.5 ,
+  noOfReviews : 1500,
+  },
+
+},
+
+{
+  id: '003',
+  image : 'images/Clothes_For_Men/c3.jpeg',
+  company : 'Marcon',
+  item_name : 'Coat For Blue ',
+  current_price : 3600,
+  original_price : 4000,
+  discount_percentage : 10,
+  return_period : 12,
+  delivery_date : '12 Mar 2024',
+  rating : {
+    stars : 4.5 ,
+    noOfReviews : 1500,
+    },
+  
+  },
+
+  {
+    id: '004',
+    image : 'images/Clothes_For_Men/c7.jpeg',
+    company : 'Marcon',
+    item_name : 'Brown Jacket ',
+    current_price : 1800,
+    original_price : 2000,
+    discount_percentage : 10,
+    return_period : 14,
+    delivery_date : '10 Mar 2024',
+    rating : {
+      stars : 4.5 ,
+      noOfReviews : 1500,
+      },
+    
+    },
+
+    {
+      id: '005',
+      image : 'images/Clothes_For_Men/c6.jpeg',
+      company : 'Denny-Marker',
+      item_name : 'Dotted Mark-Shirt ',
+      current_price : 1200,
+      original_price : 1500,
+      discount_percentage : 30,
+      rating : {
+        stars : 4.5 ,
+        noOfReviews : 1473,
+        },
+      
+      },
+
+      
+    {
+      id: '006',
+      image : 'images/Clothes_For_Women/w2.jpeg',
+      company : 'Marcon',
+      item_name : 'Jumper ',
+      current_price : 600,
+      original_price : 1000,
+      discount_percentage : 40,
+      rating : {
+        stars : 4.5 ,
+        noOfReviews : 1530,
+        },
+      
+      },
+      
+    {
+      id: '007',
+      image : 'images/Clothes_For_Women/w3.jpeg',
+      company : 'Creamson',
+      item_name : 'Cotton Cream Dress ',
+      current_price : 800,
+      original_price : 1045,
+      discount_percentage : 31,
+      rating : {
+        stars : 4.5 ,
+        noOfReviews : 1500,
+        },
+      
+      },
+      
+    {
+      id: '008',
+      image :'images/Clothes_For_Women/w5.jpeg',
+      company : 'Marcon',
+      item_name : 'Cotton Dress ',
+      current_price : 600,
+      original_price : 1045,
+      discount_percentage : 41,
+      rating : {
+        stars : 4.5 ,
+        noOfReviews : 1500,
+        },
+      
+      },
+      
+    {
+      id: '009',
+      image :'images/Clothes_For_Women/w1.jpeg',
+      company : 'Malohatra',
+      item_name : 'Cotton Lehenga ',
+      current_price : 2500,
+      original_price : 3000,
+      discount_percentage : 15,
+      rating : {
+        stars : 4.5 ,
+        noOfReviews : 1500,
+        },
+      
+      },
+      
+    {
+      id: '010',
+      image : 'images/Clothes_For_Women/w6.jpeg',
+      company : 'Blue-monkey',
+      item_name : 'Dotted Blue Kurta',
+      current_price : 1000,
+      original_price : 1500,
+      discount_percentage : 33,
+      rating : {
+        stars : 4.5 ,
+        noOfReviews : 1587,
+        },
+      
+      },
+      
+    {
+      id: '011',
+      image : 'images/Shoes_For_Men/Shoes 3.jpeg',
+      company : 'WoodWorld',
+      item_name : 'Funky World Shocker',
+      current_price : 1900,
+      original_price : 3800,
+      discount_percentage : 50,
+      rating : {
+        stars : 4.5 ,
+        noOfReviews : 1708,
+        },
+      
+      },
+      
+    {
+      id: '012',
+      image :  'images/Shoes_For_Men/Shoes 5.jpeg',
+      company : 'Marcon',
+      item_name : 'Brownny Mack Shoes ',
+      current_price : 2700,
+      original_price : 3000,
+      discount_percentage : 10,
+      rating : {
+        stars : 4.8 ,
+        noOfReviews : 1400,
+        },
+      
+      },
+      
+    {
+      id: '013',
+      image :  'images/Shoes_For_Men/Shoes 6.jpeg',
+      company : 'Marcon',
+      item_name : 'Redish Leather ',
+      current_price : 2600,
+      original_price : 3500,
+      discount_percentage : 32,
+      rating : {
+        stars : 4.5 ,
+        noOfReviews : 1900,
+        },
+      
+      },
+      
+    {
+      id: '014',
+      image :  'images/Shoes_For_Men/Shoes 4.jpeg',
+      company : 'Denny',
+      item_name : 'Cotton Shoes ',
+      current_price : 600,
+      original_price : 1045,
+      discount_percentage : 41,
+      rating : {
+        stars : 3.5 ,
+        noOfReviews : 1500,
+        },
+      
+      },
+      
+    {
+      id: '015',
+      image : 'images/Shoes_For_Men/Shoes 1.jpeg',
+      company : 'Marcon-Shoes',
+      item_name : 'Brown Leather Shoes ',
+      current_price : 1600,
+      original_price : 2045,
+      discount_percentage : 20,
+      rating : {
+        stars : 4.5 ,
+        noOfReviews : 1500,
+        },
+      
+      },
+      
+    {
+      id: '016',
+      image : 'images/Shoes_For_Women/women2.jpeg',
+      company : 'Pink',
+      item_name : ' Pure Cotton Shoes ',
+      current_price : 600,
+      original_price : 1045,
+      discount_percentage : 41,
+      rating : {
+        stars : 4.5 ,
+        noOfReviews : 1500,
+        },
+      
+      },
+      
+    {
+      id: '017',
+      image : 'images/Shoes_For_Women/women1.jpeg',
+      company : 'Barbie',
+      item_name : 'Gold Ring Lock ',
+      current_price : 2700,
+      original_price : 3000,
+      discount_percentage : 10,
+      rating : {
+        stars : 4.9 ,
+        noOfReviews : 2800,
+        },
+      
+      },
+
+      
+    {
+      id: '018',
+      image : 'images/Shoes_For_Women/women3.jpeg',
+      company : 'Black-World',
+      item_name : 'Blackish Cotton Shoes ',
+      current_price : 1200,
+      original_price : 2000,
+      discount_percentage : 40,
+      rating : {
+        stars : 4.7 ,
+        noOfReviews : 1800,
+        },
+      
+      },
+
+      
+    {
+      id: '019',
+      image : 'images/Shoes_For_Women/women4.jpeg',
+      company : 'Denny-Vox',
+      item_name : 'Jency Shoes ',
+      current_price : 900,
+      original_price : 1200,
+      discount_percentage : 25,
+      rating : {
+        stars : 3.5 ,
+        noOfReviews : 1100,
+        },
+      
+      },
+
+      
+    {
+      id: '020',
+      image : 'images/Shoes_For_Women/women7.jpeg',
+      company : 'Pink-pon',
+      item_name : 'Pure Fanccy Pink Shoes ',
+      current_price : 1600,
+      original_price : 2050,
+      discount_percentage : 43,
+      rating : {
+        stars : 4.7 ,
+        noOfReviews : 1900,
+        },
+      
+      },
+  
+]
